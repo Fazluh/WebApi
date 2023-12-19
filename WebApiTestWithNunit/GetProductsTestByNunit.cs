995289b9fe9f6d69e9372d31d5fed0952aa8343b
Fazluh <fk368414@gmail.com>
2023-12-19 10:41:11 +0530

    Add WebApiTestWithNunit in the project and add 1 test file to test the functionality of application

---
+++ WebApiTestWithNunit/GetProductsTestByNunit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApiTestWithNunit
+{
+    [TestFixture]
+    public class GetProductsTestByNunit
+    {
+        [Test]
+        public void checkProducts()
+        {
+            ProductModel model = new ProductModel();
+            model.ProdID= 1;
+            model.ProdName= "Hello";
+            model.ProdPrice = 123;
+            model.QTY = 1;
+            model.ProdDesc = "Test";
+            model.Size = 0;
+            model.Gender = 0;
+
+            var res = model;
+
+            Assert.That(model.ProdName, Is.EqualTo(res.ProdName));
+
+        }
+       
+
+    } 
+}
---
+++ WebApiTestWithNunit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WebApiTestWithNunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
